eec9bebeb9dccb8d8205bef46097e76cce176e68
Артем Будин <botak97x@gmail.com>
2025-05-22 14:13:37 +0500

    Добавьте файлы проекта.

---
+++ task_1.py
@@ -0,0 +1,5 @@
+animal = input("Enter the species of the pet: ")
+age = input("Enter the age of the pet: ")
+name = input("Enter the pet's name: ")
+
+print(f'It\'s a {animal} named "{name}". Age: {age} years.')
---
+++ task_2.py
@@ -0,0 +1,9 @@
+type_1 = "Australopithecus afarensis"
+type_2 = "Australopithecus africanus"
+type_3 = "Homo habilis"
+type_4 = "Homo erectus"
+type_5 = "Homo heidelbergensis"
+type_6 = "Homo neanderthalensis"
+type_7 = "Homo sapiens"
+
+print(type_1, type_2, type_3, type_4, type_5, type_6, type_7, sep=" => ")
